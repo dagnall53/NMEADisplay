f2746f98fc056f14723c474ab8422bd8c99a6447
dagnall <dagnall53@googlemail.com>
2025-03-27 20:28:06 +0000

    Added Batch and other files for serial programming via BATCH file

---
+++ build/esp32.esp32.esp32s3/Program_com8.bat
@@ -0,0 +1,2 @@
+esptool.exe --chip esp32s3 --port COM8 --baud 921600  --before default_reset --after hard_reset write_flash  -z --flash_mode dio --flash_freq 80m --flash_size 8MB 0x0 NMEADisplay.ino.bootloader.bin 0x8000 NMEADisplay.ino.partitions.bin 0xe000 boot_app0.bin 0x10000 NMEADisplay.ino.bin
+ 
